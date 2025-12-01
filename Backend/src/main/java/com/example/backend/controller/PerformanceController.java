83c061c59f1c0ceac53653d015e5436f69bfe35b
safirestorm <safa0003@stud.ek.dk>
2025-12-01 13:30:07 +0100

    Tilføjet getAll metode til performance og html side

--- Backend/src/main/java/com/example/backend/controller/PerformanceController.java
+++ Backend/src/main/java/com/example/backend/controller/PerformanceController.java
@@ -1,7 +1,26 @@
 package com.example.backend.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.backend.model.Performance;
+import com.example.backend.service.PerformanceService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/performances")
 public class PerformanceController {
+    private final PerformanceService performanceService;
+
+    public PerformanceController(PerformanceService performanceService) {
+        this.performanceService = performanceService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Performance>> GetAllPerformances() {
+        System.out.println(performanceService.GetAllPerformances());
+        return ResponseEntity.ok(performanceService.GetAllPerformances());
+    }
 }
