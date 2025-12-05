28487a94f3233007e3b4755661b18c45691f360b
hdtrap <peterpetertange@gmail.com>
2025-12-05 11:54:33 +0100

    Trying to fix github stuff

--- Backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ Backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 @ActiveProfiles("test")
 class BackendApplicationTests {
 
