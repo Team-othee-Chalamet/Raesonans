a13f752f39c2402887f568c2166e5a6639712091
hdtrap <peterpetertange@gmail.com>
2025-12-05 12:14:57 +0100

    Tried outcommenting initData

--- Backend/src/main/java/com/example/backend/config/InitPlayData.java
+++ Backend/src/main/java/com/example/backend/config/InitPlayData.java
@@ -1,28 +1,28 @@
-package com.example.backend.config;
-
-import com.example.backend.model.*;
-import com.example.backend.repo.CreditRepo;
-import com.example.backend.repo.ImageRepo;
-import com.example.backend.repo.PlayRepo;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-@Component
-public class InitPlayData implements CommandLineRunner {
-    private final ImageRepo imageRepo;
-    PlayRepo playRepo;
-
-    public InitPlayData(PlayRepo playRepo, ImageRepo imageRepo){
-        this.playRepo = playRepo;
-        this.imageRepo = imageRepo;
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
-    }
-}
+//package com.example.backend.config;
+//
+//import com.example.backend.model.*;
+//import com.example.backend.repo.CreditRepo;
+//import com.example.backend.repo.ImageRepo;
+//import com.example.backend.repo.PlayRepo;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@Component
+//public class InitPlayData implements CommandLineRunner {
+//    private final ImageRepo imageRepo;
+//    PlayRepo playRepo;
+//
+//    public InitPlayData(PlayRepo playRepo, ImageRepo imageRepo){
+//        this.playRepo = playRepo;
+//        this.imageRepo = imageRepo;
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//
+//    }
+//}
--- Backend/src/main/java/com/example/backend/config/InitUserConfig.java
+++ Backend/src/main/java/com/example/backend/config/InitUserConfig.java
@@ -1,31 +1,31 @@
-package com.example.backend.config;
-
-import com.example.backend.model.AppUser;
-import com.example.backend.repo.AppUserRepo;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
-@Configuration
-public class InitUserConfig {
-
-    @Bean
-    CommandLineRunner loadUserTestData(
-            AppUserRepo appUserRepo
-    ) {
-        return args -> {
-            appUserRepo.deleteAll();
-
-            if (appUserRepo.count() == 0) {
-                AppUser Peter = new AppUser("Peter", "Test");
-                AppUser Frederik = new AppUser("Frederik", "Test");
-                AppUser Sarah = new AppUser("Sarah", "Test");
-                AppUser Mikkel = new AppUser("Mikkel", "Test");
-
-                appUserRepo.saveAll(List.of(Peter, Frederik, Sarah, Mikkel));
-            }
-        };
-    }
-}
+//package com.example.backend.config;
+//
+//import com.example.backend.model.AppUser;
+//import com.example.backend.repo.AppUserRepo;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.List;
+//
+//@Configuration
+//public class InitUserConfig {
+//
+//    @Bean
+//    CommandLineRunner loadUserTestData(
+//            AppUserRepo appUserRepo
+//    ) {
+//        return args -> {
+//            appUserRepo.deleteAll();
+//
+//            if (appUserRepo.count() == 0) {
+//                AppUser Peter = new AppUser("Peter", "Test");
+//                AppUser Frederik = new AppUser("Frederik", "Test");
+//                AppUser Sarah = new AppUser("Sarah", "Test");
+//                AppUser Mikkel = new AppUser("Mikkel", "Test");
+//
+//                appUserRepo.saveAll(List.of(Peter, Frederik, Sarah, Mikkel));
+//            }
+//        };
+//    }
+//}
