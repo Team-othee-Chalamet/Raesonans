f4224f528757c8d26d892281c7d7871d56f6e2d7
hdtrap <peterpetertange@gmail.com>
2025-12-08 10:01:32 +0100

    Noticed I am in main, would like to not be

--- Backend/src/main/java/com/example/backend/util/Hasher.java
+++ Backend/src/main/java/com/example/backend/util/Hasher.java
@@ -3,6 +3,19 @@ package com.example.backend.util;
 public class Hasher {
 
     public static String hashPassword(String password) {
-        return password;
+        StringBuilder sb = new StringBuilder();
+
+        int modifier = password.length();
+        int tempMod;
+
+        for (char c : password.toCharArray()) {
+            tempMod = (int) c;
+            if (modifier > tempMod)
+            {sb.append(modifier % tempMod);}
+            else
+            {sb.append(tempMod % modifier);}
+            }
+
+        return sb.toString();
     }
 }
