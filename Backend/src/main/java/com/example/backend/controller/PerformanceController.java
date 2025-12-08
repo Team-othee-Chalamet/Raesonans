581763c6633ca90bd6055157dfd3d065b4da9d78
safirestorm <safa0003@stud.ek.dk>
2025-12-08 13:33:22 +0100

    Tilføjet CRUD endpoints i PerformanceController

--- Backend/src/main/java/com/example/backend/controller/PerformanceController.java
+++ Backend/src/main/java/com/example/backend/controller/PerformanceController.java
@@ -2,10 +2,9 @@ package com.example.backend.controller;
 
 import com.example.backend.dto.PerformanceDto;
 import com.example.backend.service.PerformanceService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,9 +18,32 @@ public class PerformanceController {
     }
 
     @GetMapping
-    public ResponseEntity<List<PerformanceDto>> GetAllPerformances() {
+    public ResponseEntity<List<PerformanceDto>> getAllPerformances() {
         System.out.println(performanceService.getAllPerformances());
         return ResponseEntity.ok(performanceService.getAllPerformances());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<PerformanceDto> getPerformanceById(@PathVariable Long performanceId) {
+        return ResponseEntity.ok(performanceService.getPerformanceById(performanceId));
+    }
+
+    @PostMapping
+    public ResponseEntity<PerformanceDto> createPerformance(@RequestBody PerformanceDto performanceDto){
+        return ResponseEntity.ok(performanceService.createPerformance(performanceDto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PerformanceDto> updatePerformance(@PathVariable Long performanceId, @RequestBody PerformanceDto performanceDto) {
+        try {
+            return ResponseEntity.ok(performanceService.editPerformance(performanceId, performanceDto));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePerformanceById(@PathVariable Long performanceId) {
+        performanceService.deletePerformance(performanceId);
+    }
 }
