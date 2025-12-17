6a1be48162e08e931424d749b302706dd78d6a36
mimi0001 <mimi0001@stud.ek.dk>
2025-12-17 10:04:11 +0100

    Basic image upload available, further integration still needed

--- Frontend/Scripts/images.js
+++ Frontend/Scripts/images.js
@@ -1,19 +1,60 @@
 import {get,post,put,del} from "../Scripts/fetchUtil.js";
 document.addEventListener("DOMContentLoaded", (event) => initApp(event));
 
+const listOfTitles = [
+        "nemt",
+        "støj"
+    
+]
 
 async function initApp(event){
     const images = await getImages();
 
+    getInfo();
+
     console.log(images);
 
-    const newImage = document.createElement("img");
+    images.forEach((element) => renderImageCard(element))
 
-    newImage.src = "http://127.0.0.1:8080" + images[0].url;
+}
 
-    document.body.appendChild(newImage);
+async function getInfo(){
+    //Get list of titles from the backend and set the list.
+}
+
+function renderImageCard(imageDto){
+    console.log("Rendering image card:" + imageDto.url);
+    
+    const images = document.getElementById("images");
+
+    const template = document.getElementById("cardTemplate");
+    const clone = template.content.cloneNode(true);
+
+    const card = addInfoToCard(imageDto, clone);
 
     
+
+    images.appendChild(card);
+
+}
+
+function addInfoToCard(imageDto, card){
+    card.getElementById("imgPreview").src = "http://127.0.0.1:8080/api" + imageDto.url;
+
+    return card;
+}
+
+function addTheaterTitlesToDropDown(div){
+    //Fetch the titles from the list
+
+    listOfTitles.forEach(element => {
+        const newOption = document.createElement("option");
+        Option.name = element;
+        Option.id = element;
+
+        div.appendChild(newOption);
+    });
+
 }
 
 async function getImages(){
